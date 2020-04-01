095586097cbe2d4262cb1375de1e32fc073b6d2d
Gabriel <jgpaz5@gmail.com>
2020-04-01 15:24:24 +0200

    Organized files, Installed redux, axios, and thunk

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
-import Login from './Login';
-import Home from './Home';
-import NetWorthForm from './NetWorthForm';
-import NetWorthDetail from './NetWorthDetail';
-import SignUp from './SignUp';
+import Login from './Components/Login/Login';
+import Home from './Components/Home/Home';
+import NetWorthForm from './Components/NetWorthForm/NetWorthForm';
+import NetWorthDetail from './Components/NetWorthDetail/NetWorthDetail';
+import SignUp from './Components/SignUp/SignUp';
 
 function App() {
   return (
