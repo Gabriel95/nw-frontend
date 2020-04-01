314842a6cd1d0b4944b4df9fcd0be347cdf8aa88
Gabriel <jgpaz5@gmail.com>
2020-04-01 09:41:39 +0200

    Added a Card deck to home component.

--- src/Home.js
+++ src/Home.js
@@ -6,7 +6,43 @@ class Home extends React.Component {
     return (
       <div>
         <Navbar />
-        Home!
+        <div className="container">
+          <h2 className="mb-3">
+            Current Net Worth <i className="fas fa-chart-line ml-1"></i>
+          </h2>
+          <div className="card-deck">
+            <div className="card text-center">
+              <div className="card-body">
+                <h4 className="card-title">$100,000,000.00</h4>
+                <p className="card-text">Your current net worth.</p>
+                <p className="card-text">
+                  <small className="text-muted">01/04/2020</small>
+                </p>
+              </div>
+            </div>
+            <div className="card text-center">
+              <div className="card-body">
+                <h4 className="card-title">100%</h4>
+                <p className="card-text">
+                  Increase from your previous net worth.
+                </p>
+                <p className="card-text">
+                  <small className="text-muted">31/03/2020</small>
+                </p>
+              </div>
+            </div>
+            <div className="card text-center">
+              <div className="card-body">
+                <button className="btn btn-info btn-block card-title">
+                  <i className="fas fa-plus mr-1"></i>Calculate Net Worth
+                </button>
+                <p className="card-text">
+                  Click here to calculate your current Net Worth.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
